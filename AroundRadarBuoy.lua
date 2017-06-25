5e4c7c085bc20e76064debba65283d57b8e53bb3
鳥羽亀次郎 <tvagames@gmail.com>
2017-06-25 13:24:44 +0900

    Create AroundRadarBuoy.lua

---
+++ AroundRadarBuoy.lua
@@ -0,0 +1,47 @@
+HEIGHT_OFFSET = 100 -- 目標の頭上何mを回るか
+AROUND_DISTANCE = 200 -- 周回半径
+REGULATOR_COUNT = 3 -- ミサイルのRegulatorの数
+FUEL_TANK = 5 -- ミサイルのFuel tankの数
+TPS = 50 -- Variable ThrusterのThrust per second
+TANK_CAPA = 5000
+
+function Update(I)
+  I:ClearLogs()
+  tc = I:GetNumberOfTargets(0)
+
+  if tc > 0 then
+    -- 敵がいるとき
+    ti = I:GetTargetInfo(0, 0)
+    tp = ti.Position
+    tp.y = tp.y + HEIGHT_OFFSET
+    
+    fuelTime = FUEL_TANK * TANK_CAPA / TPS
+    
+    lifeTime = Mathf.Min(fuelTime, 60 + REGULATOR_COUNT * 180)
+    time = I:GetTime()
+    transCount = I:GetLuaTransceiverCount()
+    for transIndex = 0, transCount - 1, 1 do
+      mc = I:GetLuaControlledMissileCount(transIndex)
+      for mi = 0, mc - 1, 1 do
+        m = I:GetLuaControlledMissileInfo(transIndex,mi)
+        if m.TimeSinceLaunch > lifeTime then
+          I:LogToHud("レーダーブイ寿命につき自爆。生存時間は" .. m.TimeSinceLaunch .. "秒でした")
+          I:DetonateLuaControlledMissile(transIndex,mi)
+        elseif m.Position.y < 0 then
+          -- 水没したら自爆
+          I:LogToHud("レーダーブイ水没につき自爆。生存時間は" .. m.TimeSinceLaunch .. "秒でした")
+          I:DetonateLuaControlledMissile(transIndex,mi)
+        else
+          mpos = m.Position
+          mpos.y = tp.y
+          -- 目標から見たミサイルへのベクトル
+          v = mpos - tp 
+          vn = v.normalized
+          p = Quaternion.Euler(0, 45, 0) * vn
+          aimpos = tp + (p * AROUND_DISTANCE)
+          I:SetLuaControlledMissileAimPoint(transIndex,mi,aimpos.x,tp.y,aimpos.z)
+        end
+      end
+    end
+  end
+end
